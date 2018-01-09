768e2635dd478231d7b252b9b5244344cca21d6b
Natalee C <COPENATALEE@ME.COM>
2018-01-09 14:01:46 -0500

    mLab Database creation

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,16 @@ mongoose.Promise = Promise
 
 const mongoUri = 'mongodb://localhost/routines'
 
-mongoose
-  .connect(mongoUri, {useMongoClient: true})
-  .then(connection => console.log(`Connection established to db '${connection.db.databaseName}'`))
-  .catch(connectionError => console.log('Connection failed!', connectionError))
+if (process.env.NODE_ENV == "production") {
+  mongoose
+    .connect(process.env.MLAB_URL)
+    .then(connection => console.log(`Established connection to database '${connection.db.databaseName}'`))
+    .catch(connectionError => console.log('Database connection failed!', connectionError))
+} else {
+  mongoose
+    .connect(mongoUri)
+    .then(connection => console.log(`Connected development database '${connection.db.databaseName}'`))
+    .catch(connectionError => console.log('Development database has failed!', connectionError))
+}
 
   module.exports = mongoose //because we're importing a connected mongoose instance into our `seed.js`
